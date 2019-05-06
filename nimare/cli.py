3057862ff625e641ce9968d0ed3e7cc72d282fb6
62442katieb <kbott006@fiu.edu>
2019-05-06 11:46:54 -0400

    add metacluster to cli

--- nimare/cli.py
+++ nimare/cli.py
@@ -3,7 +3,7 @@ import click
 from nimare.workflows.ale import ale_sleuth_workflow
 from nimare.workflows.ibma_perm import con_perm_workflow
 from nimare.workflows.macm import macm_workflow
-# from nimare.workflows.cluster import meta_cluster_workflow
+from nimare.workflows.cluster import meta_cluster_workflow
 # from nimare.workflows.scale import scale_workflow
 from nimare.workflows.peaks2maps import peaks2maps_workflow
 from nimare.workflows.conversion import sleuth2nimare, neurosynth2nimare
@@ -19,7 +19,7 @@ def cli():
 cli.add_command(ale_sleuth_workflow)
 cli.add_command(con_perm_workflow)
 cli.add_command(macm_workflow)
-# cli.add_command(meta_cluster_workflow)
+cli.add_command(metacluster)
 # cli.add_command(scale_workflow)
 cli.add_command(peaks2maps_workflow)
 cli.add_command(sleuth2nimare)
--- nimare/workflows/__init__.py
+++ nimare/workflows/__init__.py
@@ -6,7 +6,7 @@ from .ale import ale_sleuth_workflow
 from .ibma_perm import con_perm_workflow
 from .macm import macm_workflow
 from .peaks2maps import peaks2maps_workflow
-from .cluster import metacluster
+from .cluster import meta_cluster_workflow
 
 __all__ = ['ale_sleuth_workflow', 'con_perm_workflow', 'macm_workflow',
-           'peaks2maps_workflow', 'metacluster']
+           'peaks2maps_workflow', 'meta_cluster_workflow']
